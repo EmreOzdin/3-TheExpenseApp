d42424c2f72834021a052104c1ef133e5eba4f73
Emre Özdin <mrzdnboun@gmail.com>
2024-03-01 17:50:48 +0300

    Update expenses data and add ExpensesSummary component

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,12 +1,45 @@
 import { View } from "react-native";
 
-import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import ExpensesSummary from "./ExpensesSummary";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2023-03-01"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2023-02-29"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-02-28"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2023-02-15"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2023-02-05"),
+  },
+];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
