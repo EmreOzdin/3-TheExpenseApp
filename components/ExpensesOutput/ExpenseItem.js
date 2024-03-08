e6c872bae6e40f54d77052a1b96b4d6aa603a418
Emre Özdin <mrzdnboun@gmail.com>
2024-03-08 14:02:18 +0300

    Update navigation parameters for `ManageExpense` screen

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -4,11 +4,11 @@ import { useNavigation } from "@react-navigation/native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
-const ExpenseItem = ({ description, amount, date }) => {
+const ExpenseItem = ({ id, description, amount, date }) => {
   const navigation = useNavigation();
 
   function expensePressHandler() {
-    navigation.navigate("ManageExpense");
+    navigation.navigate("ManageExpense", { expenseId: id });
   }
   return (
     <Pressable
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,15 @@
 import { Text } from "react-native";
-function ManageExpense() {
+import { useLayoutEffect } from "react";
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>ManageExpense Screen</Text>;
 }
 
