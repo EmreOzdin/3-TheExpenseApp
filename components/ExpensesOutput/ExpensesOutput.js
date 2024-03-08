4e46c1caaef2d85672e76bd8dd26b2fef215133a
Emre Özdin <mrzdnboun@gmail.com>
2024-03-08 11:57:43 +0300

    Added new expenses to DUMMY_EXPENSES and updated styles.

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -35,6 +35,30 @@ const DUMMY_EXPENSES = [
     amount: 18.59,
     date: new Date("2023-02-05"),
   },
+  {
+    id: "e6",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2023-02-29"),
+  },
+  {
+    id: "e7",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-02-28"),
+  },
+  {
+    id: "e8",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2023-02-15"),
+  },
+  {
+    id: "e9",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2023-02-05"),
+  },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -50,8 +74,10 @@ export default ExpensesOutput;
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 24,
+    paddingBottom: 0,
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
     backgroundColor: GlobalStyles.colors.primary700,
   },
 });
