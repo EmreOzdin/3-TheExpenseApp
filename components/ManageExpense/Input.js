afc472a30ba9c177bf106fc4b92022a60a8b8bff
Emre Özdin <mrzdnboun@gmail.com>
2024-03-24 14:39:50 +0300

    bug fixed

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { GlobalStyles } from "../../constants/styles";
+import { GlobalStyles } from '../../constants/styles';
 
 function Input({ label, invalid, style, textInputConfig }) {
+
   const inputStyles = [styles.input];
 
   if (textInputConfig && textInputConfig.multiline) {
-    inputStyles.push(styles.inputMultiline);
+    inputStyles.push(styles.inputMultiline)
   }
 
   if (invalid) {
@@ -15,9 +16,7 @@ function Input({ label, invalid, style, textInputConfig }) {
 
   return (
     <View style={[styles.inputContainer, style]}>
-      <Text style={[styles.label, invalid && styles.invalidLabel]}>
-        {label}
-      </Text>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>{label}</Text>
       <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
@@ -28,7 +27,7 @@ export default Input;
 const styles = StyleSheet.create({
   inputContainer: {
     marginHorizontal: 4,
-    marginVertical: 8,
+    marginVertical: 8
   },
   label: {
     fontSize: 12,
@@ -44,12 +43,12 @@ const styles = StyleSheet.create({
   },
   inputMultiline: {
     minHeight: 100,
-    textAlignVertical: "top",
+    textAlignVertical: 'top'
   },
   invalidLabel: {
-    color: GlobalStyles.colors.error500,
+    color: GlobalStyles.colors.error500
   },
   invalidInput: {
-    backgroundColor: GlobalStyles.colors.error50,
-  },
+    backgroundColor: GlobalStyles.colors.error50
+  }
 });
